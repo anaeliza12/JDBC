cc7c7d644b187485d8ba1d55fa7a294a76a8ffde
Ana Eliza <anaelizaperobelli@gmail.com>
2023-07-22 19:15:13 -0300

    Inserção de dados pelo usuário

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,36 +1,91 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
 
 import db.DB;
 
 public class Program {
 	public static void main(String[] args) {
 
+		Scanner tec = new Scanner(System.in);
+		SimpleDateFormat stf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection con = null;
-		Statement st = null;
-		ResultSet rs = null;
+		PreparedStatement ps = null;
 
 		try {
-
 			con = DB.getConnection();
-			st = con.createStatement();
-			rs = st.executeQuery("SELECT * FROM department");
-		
-		
-			while(rs.next()) {
-				System.out.println(rs.getInt("Id") + " " + rs.getString("Name" ));
-				
-			
-			}
-		
+			String script = "INSERT INTO SELLER " + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
+					+ "VALUES(?,?,?,?,?) ";
+			ps = con.prepareStatement(script, Statement.RETURN_GENERATED_KEYS);
+
+			System.out.println("Nome: ");
+			ps.setString(1, tec.nextLine());
+
+			System.out.println("Email: ");
+			ps.setString(2, tec.nextLine());
+
+			System.out.println("Data : ");
+			ps.setDate(3, new java.sql.Date(stf.parse(tec.next()).getTime()));
+
+			System.out.println("Salary : ");
+			ps.setDouble(4, tec.nextDouble());
+
+			System.out.println("Department : ");
+			ps.setInt(5, tec.nextInt());
+
+			int rows = ps.executeUpdate();
+			if (rows != 0) {
+				ResultSet rs = ps.getGeneratedKeys();
+
+				while (rs.next()) {
+
+					System.out.println(rs.getInt(1));
+				}
+
+			} else
+				System.out.println("No Rows affected");
 
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
+		} catch (ParseException e) {
+			System.out.println(e.getMessage());
 		}
 
+		finally {
+
+			DB.closeStatemnt(ps);
+			DB.closeConnection();
+		}
 	}
 }
+//		Statement st = null;
+//		ResultSet rs = null;
+//
+//		try {
+//
+//			con = DB.getConnection();
+//			st = con.createStatement();
+//			rs = st.executeQuery("SELECT * FROM seller ");
+//
+//			while (rs.next()) {
+//				System.out.println(rs.getInt("Id") + " " + rs.getString("Name") );
+//
+//			}
+//
+//		} catch (SQLException e) {
+//			System.out.println(e.getMessage());
+//		}
+//
+//		finally {
+//
+//			DB.closeStatemnt(st);
+//			DB.closeResultSet(rs);
+//			DB.closeConnection();
+//		}
--- src/db/DB.java
+++ src/db/DB.java
@@ -1,44 +1,40 @@
 package db;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
-	
+
 	private static Connection con = null;
-	
-//	static final String DB_URL = "jdbc:mysql://localhost:3306/coursejdbc?allowPublicKeyRetrieval=true&useSSL=false";
-	
+
 	public static Connection getConnection() {
-		 if(con == null) {
-			 try {
-				 
-			Properties props = loadProperties();
-			String url = props.getProperty("dburl");
-			con = DriverManager.getConnection(url, props);
-			 }catch(SQLException e) {
-				 throw new DbException(e.getMessage());
-			 }
+		if (con == null) {
+			try {
+
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				con = DriverManager.getConnection(url, props);
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
 		}
-		 return con;
+		return con;
 	}
-	
-	
-	
+
 	private static Properties loadProperties() {
-		
-	
-		try (FileInputStream fs = new FileInputStream("db.properties")){
+
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
 			Properties props = new Properties();
 			props.load(fs);
-			
+
 			return props;
-		}catch(IOException e) {
+		} catch (IOException e) {
 			throw new DbException(e.getMessage());
 		}
 	}
@@ -47,11 +43,33 @@ public class DB {
 		try {
 
 			if (con != null) {
-			con.close();
-		}
-		}catch(SQLException e) {
+				con.close();
+			}
+		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
 		}
 	}
-	
+
+	public static void closeStatemnt(Statement st) {
+
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+
+				e.printStackTrace();
+			}
+		}
+	}
+
 }
